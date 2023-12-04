4c4271de4954910c3b8b1bfecb84156131b65f9a
Nicolas Descostes <nicolas.descostes@embl.it>
2023-12-04 13:58:53 +0100

    remove spaces in export

--- recipes/drompa/build.sh
+++ recipes/drompa/build.sh
@@ -2,4 +2,4 @@
 
 git clone git@github.com:coherentgraphics/cpdf-binaries.git
 
-export PATH = $PATH:$HOME/DROMPA3:$HOME/cpdf-binaries/Linux-Intel-64bit/
+export PATH=$PATH:$HOME/DROMPA3:$HOME/cpdf-binaries/Linux-Intel-64bit/
